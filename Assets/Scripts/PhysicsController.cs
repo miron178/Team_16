43ed4c0d2d76809fe2710c124def742ec37ff1fc
Miron Bury <s1906443@glos.ac.uk>
2020-01-25 19:28:35 +0000

    seporate controles

--- Assets/Scripts/PhysicsController.cs
+++ Assets/Scripts/PhysicsController.cs
@@ -2,25 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhysicsController : MonoBehaviour
-{
+public class PhysicsController : MonoBehaviour {
 	[SerializeField]
 	private float acceleration = 100000;
 	private Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
+	[SerializeField]
+	private string prefix = "P1";
+	// Start is called before the first frame update
+	void Start() {
 		rb = GetComponent<Rigidbody>();
-    }
+	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-		float horizontal = acceleration * Input.GetAxis("Horizontal");
-		float vertical   = acceleration * Input.GetAxis("Vertical");
+	// Update is called once per frame
+	void FixedUpdate() {
+		float horizontal = acceleration * Input.GetAxis(prefix + "Horizontal");
+		float vertical = acceleration * Input.GetAxis(prefix + "Vertical");
 
 		rb.AddForce(
 			Vector3.right * horizontal * Time.fixedDeltaTime +
 			Vector3.forward * vertical * Time.fixedDeltaTime);
-    }
+	}
 }
