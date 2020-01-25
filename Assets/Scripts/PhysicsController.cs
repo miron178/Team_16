3e387508f259176e6ccc24d1f0cea2b587cd2769
Miron Bury <s1906443@glos.ac.uk>
2020-01-25 19:01:23 +0000

    char controller

---
+++ Assets/Scripts/PhysicsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+	[SerializeField]
+	private float acceleration = 100000;
+	private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		float horizontal = acceleration * Input.GetAxis("Horizontal");
+		float vertical   = acceleration * Input.GetAxis("Vertical");
+
+		rb.AddForce(
+			Vector3.right * horizontal * Time.fixedDeltaTime +
+			Vector3.forward * vertical * Time.fixedDeltaTime);
+    }
+}
