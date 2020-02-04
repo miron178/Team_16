ff40660cf27d0cec9ec1e6e5bbe63c153adadba7
[     ] <miron.bury@gmail.com>
2020-02-04 13:25:01 +0000

    jump script
    currently fly

--- Assets/Scripts/PhysicsController.cs
+++ Assets/Scripts/PhysicsController.cs
@@ -14,6 +14,8 @@ public class PhysicsController : MonoBehaviour {
 	private float rotationSpeed = 1f;
 	[SerializeField]
 	private float maxSpeed = 1f;
+	[SerializeField]
+	private float jumpForce = 100000f;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -22,15 +24,22 @@ public class PhysicsController : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate() {
-		float horizontal = rotation   * Input.GetAxis(prefix + "Horizontal");
-		float vertical = acceleration * Input.GetAxis(prefix + "Vertical");
+		float horizontal = rotation     * Input.GetAxis(prefix + "Horizontal");
+		float vertical   = acceleration * Input.GetAxis(prefix + "Vertical");
+		float jump       = jumpForce * Input.GetAxis(prefix + "Jump");
 
+		//move left and right
 #if true
 		rb.AddRelativeForce(Vector3.right * horizontal * Time.fixedDeltaTime);
 #endif
 		rb.AddRelativeForce(Vector3.forward * vertical * Time.fixedDeltaTime);
+		//rotate left and right
 #if false
 		rb.AddRelativeTorque(Vector3.up * horizontal * Time.fixedDeltaTime);
+#endif
+		//Jump
+#if true
+		rb.AddRelativeForce(Vector3.up * jump * Time.fixedDeltaTime);
 #endif
 	}
 
