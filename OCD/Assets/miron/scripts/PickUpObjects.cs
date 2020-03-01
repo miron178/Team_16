5c46052d31fbdf567857790781c4e97fae927797
Miron Bury <s1906443@glos.ac.uk>
2020-03-01 20:40:36 +0000

    remake pickup
    lost changes from 17th
    remade pickup from ground up

---
+++ OCD/Assets/miron/scripts/PickUpObjects.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObjects : MonoBehaviour
+{
+	[SerializeField]
+	private Transform des;
+	private GameObject objectInRange = null;
+	private GameObject holding = null;
+
+	// Update is called once per frame
+	void Update()
+    {
+		//pick up
+		if (Input.GetKey(KeyCode.E) && !holding && objectInRange) {
+			PickUp(objectInRange);
+			holding = objectInRange;
+			objectInRange = null;
+		}
+
+		//drop
+		if (Input.GetKey(KeyCode.R) && holding) {
+			Drop(holding);
+			objectInRange = holding;
+			holding = null;
+		}
+	}
+
+	private void PickUp(GameObject gameObject) {
+		gameObject.GetComponent<Rigidbody>().isKinematic = true;
+		gameObject.transform.position = des.position;
+		gameObject.transform.parent = des;
+	}
+	private void Drop(GameObject gameObject) {
+		gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		//gameObject.transform.position = unchanged
+		gameObject.transform.parent = null;
+	}
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			objectInRange = other.gameObject;
+		}
+	}
+	private void OnTriggerExit(Collider other) {
+
+		if (other.gameObject == objectInRange) {
+			objectInRange = null;
+		}
+	}
+}
+
