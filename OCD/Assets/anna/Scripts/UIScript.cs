4f64b90b46f3f4b6add3f3aab122be6d6824eb10
Anna Morgan <34129238+annamorgan17@users.noreply.github.com>
2020-04-06 23:52:10 +0100

    subtle chnages
    shortened dining room, random gen plates and toys, added time to bedroom

--- OCD/Assets/anna/Scripts/UIScript.cs
+++ OCD/Assets/anna/Scripts/UIScript.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class timecounter : MonoBehaviour
+public class UIScript : MonoBehaviour
 {
     public Text startText;
     float timeLeft = 216.0f;
     void Update()
     {
+        timeCounter();
+    }
+
+    void timeCounter()
+    {
         timeLeft -= Time.deltaTime;
-        startText.text = "Time: "+(timeLeft/60).ToString("0.00");
+        startText.text = "Time: " + (timeLeft / 60).ToString("0.00");
         if (timeLeft < 0)
         {
             gameObject.SetActive(false);
