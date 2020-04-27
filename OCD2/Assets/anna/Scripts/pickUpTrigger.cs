8bb88d417a0b121c53d5b4879cbdd15ba2cf3307
Anna Morgan <34129238+annamorgan17@users.noreply.github.com>
2020-04-27 20:19:20 +0100

    attempt again

---
+++ OCD2/Assets/anna/Scripts/pickUpTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpTrigger : MonoBehaviour
+{
+    public pickup _pickup;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Player2" || 
+        other.gameObject.tag == "Player3" || other.gameObject.tag == "Player4" )
+        {
+            _pickup.isInRange(); 
+        }
+       
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Player2" ||
+        other.gameObject.tag == "Player3" || other.gameObject.tag == "Player4")
+        {
+            _pickup.isNotInRange();
+        }
+    }
+}
---
+++ OCD2/Assets/anna/Scripts/timecounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timecounter : MonoBehaviour
+{
+    public Text startText;
+    float timeLeft = 216.0f;
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        startText.text = "Time: "+(timeLeft/60).ToString("0.00");
+        if (timeLeft < 0)
+        {
+            gameObject.SetActive(false);
+            //mum comes home
+        }
+    }
+}
