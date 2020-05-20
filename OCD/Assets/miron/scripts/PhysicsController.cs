9119dc462ad9efc7bed36611e641c1376aec5418
Miron Bury <miron.bury@gmail.com>
2020-05-20 19:43:48 +0100

    merged pickup and physics controller scripts

--- OCD/Assets/miron/scripts/PhysicsController.cs
+++ OCD/Assets/miron/scripts/PhysicsController.cs
@@ -16,10 +16,40 @@ public class PhysicsController : MonoBehaviour {
 	private GameObject jumpCheck;
 	private int groundLayer;
 
+	[SerializeField]
+	private Transform des;
+	private GameObject objectInRange = null;
+	private GameObject holding = null;
+	private int pickUpLayer;
+	private int wallLayer;
+
+
 	// Start is called before the first frame update
 	void Start() {
 		rb = GetComponent<Rigidbody>();
-		groundLayer = LayerMask.NameToLayer("Ground"); 
+		groundLayer = LayerMask.NameToLayer("Ground");
+
+		pickUpLayer = LayerMask.NameToLayer("PickUp");
+		wallLayer = LayerMask.NameToLayer("Wall");
+	}
+
+	void Update() {
+		float interact = Input.GetAxis(prefix + "Interact");
+
+		//pick up
+		if (interact > 0 && !holding && objectInRange) {
+			PickUp(objectInRange);
+			holding = objectInRange;
+			objectInRange = null;
+		}
+
+		//drop
+		bool canDrop = !Physics.Linecast(transform.position, des.transform.position, 1 << wallLayer);
+		if (interact < 0 && holding && canDrop) {
+			Drop(holding);
+			objectInRange = holding;
+			holding = null;
+		}
 	}
 
 	// Update is called once per frame
@@ -41,6 +71,31 @@ public class PhysicsController : MonoBehaviour {
 	void LateUpdate() {
 		if (rb.velocity.magnitude > maxSpeed) {
 			rb.velocity = rb.velocity.normalized * maxSpeed;
+		}
+	}
+
+	private void PickUp(GameObject gameObject) {
+		gameObject.GetComponent<Rigidbody>().isKinematic = true;
+		gameObject.transform.position = des.position;
+		gameObject.transform.parent = des;
+	}
+
+	private void Drop(GameObject gameObject) {
+		gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		//gameObject.transform.position = unchanged
+		gameObject.transform.parent = null;
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == pickUpLayer) {
+			objectInRange = other.gameObject;
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+
+		if (other.gameObject == objectInRange) {
+			objectInRange = null;
 		}
 	}
 }
