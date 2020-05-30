dbfe2ef0ec524c9fa95fd7cee6b9e0ae58800c09
Anna Morgan <34129238+annamorgan17@users.noreply.github.com>
2020-05-30 21:56:22 +0100

    puddle fixes

--- OCD/Assets/anna/Scripts/wrench.cs
+++ OCD/Assets/anna/Scripts/wrench.cs
@@ -5,31 +5,36 @@ using UnityEngine;
 
 public class wrench : MonoBehaviour
 {//link to scripts
-    public puddleCheck puddle; 
+    public puddleCheck puddleScript; 
     public ScoreManager score;
+    public GameObject puddle;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "wrench")//if object which triggered is tagged as wrench
         {
-            puddle.nutAttached(); //run script
+            puddleScript.nutAttached(); //run script
 
-            PickUp pickUp = other.gameObject.GetComponent<PickUp>();//get the prefix of the held object
-            if (pickUp.playerPrefix == "P1") //if the prefix is player 1
+            if(puddle.activeSelf == true)
             {
-                score.IncreaseScore(1, 30);//tell the score manager and increaase by 30
+                PickUp pickUp = other.gameObject.GetComponent<PickUp>();//get the prefix of the held object
+                if (pickUp.playerPrefix == "P1") //if the prefix is player 1
+                {
+                    score.IncreaseScore(1, 30);//tell the score manager and increaase by 30
+                }
+                else if (pickUp.playerPrefix == "P2") //if the prefix is player 2
+                {
+                    score.IncreaseScore(2, 30);//tell the score manager and increaase by 30
+                }
+                else if (pickUp.playerPrefix == "P3") //if the prefix is player 3
+                {
+                    score.IncreaseScore(3, 30);//tell the score manager and increaase by 30
+                }
+                else if (pickUp.playerPrefix == "P4") //if the prefix is player 4
+                {
+                    score.IncreaseScore(4, 30);//tell the score manager and increaase by 30
+                }
             }
-            else if (pickUp.playerPrefix == "P2") //if the prefix is player 2
-            {
-                score.IncreaseScore(2, 30);//tell the score manager and increaase by 30
-            }
-            else if (pickUp.playerPrefix == "P3") //if the prefix is player 3
-            {
-                score.IncreaseScore(3, 30);//tell the score manager and increaase by 30
-            }
-            else if (pickUp.playerPrefix == "P4") //if the prefix is player 4
-            {
-                score.IncreaseScore(4, 30);//tell the score manager and increaase by 30
-            }
+           
         }
     }
 }
